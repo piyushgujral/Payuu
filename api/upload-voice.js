1318700a56c0873bdda41bb815c822d9f804c3d9
piyushgujral <piyushgujral04@gmail.com>
2026-09-18 13:03:08 +0530

    Return voice object key for reliable same-origin playback

--- api/upload-voice.js
+++ api/upload-voice.js
@@ -103,6 +103,7 @@ export default async function handler(req, res) {
     return res.status(200).json({
       success: true,
       voiceUrl: url,
+      voiceKey: key,
       key,
     });
   } catch (error) {
